bbbf83f5be1342db13383a4c4d8562fcf11619eb
fivemo <wish5213@gmail.co>
2026-06-11 21:56:36 +0800

    Revert "新增圖片"
    This reverts commit 62aded8523877ee75b05e236bd2b4bede8232311.

--- turntable.gd
+++ turntable.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 #定義圖片
 @onready var turn_all: TextureRect = $mask/reel/all
@@ -11,11 +11,13 @@ extends Node2D
 @onready var reel = $mask/reel
 
 #定義各項函數
-var photo_high:float = 293.5
-var turn_list:Array = []
+var photo_high:float = 290
+var turn_list:Array = [0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,
+0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,
+2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,
+0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4]
 var is_reeling:bool = false
 var reel_tween : Tween
-var prize_number : int = 0
 var spin_tween : Tween
 
 @onready var TurnName:Dictionary ={
@@ -28,16 +30,12 @@ var spin_tween : Tween
 
 
 func _ready() -> void:
-		_reel_photo()
-		start_spin()
+	#_reel_photo(turn_list)
+	#start_spin()
+	pass
 
-func _reel_photo():
-	#
-	for i in range(70):
-		turn_list.append(i % 5)
-		turn_list.append(randi_range(0,4)) 
-	
-	turn_list.shuffle()
+func _reel_photo(master_array : Array):
+	turn_list = master_array.duplicate()
 	
 	for i in range(turn_list.size()):
 		var tex = TextureRect.new()
@@ -46,6 +44,8 @@ func _reel_photo():
 		reel.add_child(tex)
 	
 	reel.position.y = -(photo_high * turn_list.size())
+	
+	start_spin()
 
 #輪盤旋轉
 func start_spin():
@@ -54,7 +54,7 @@ func start_spin():
 		return
 	is_reeling = true
 	reel_tween = create_tween()
-	reel_tween.tween_property(reel,"position:y", reel.position.y + (photo_high * turn_list.size()), 13)
+	reel_tween.tween_property(reel,"position:y", reel.position.y + (photo_high * turn_list.size()), 15)
 
 #輪盤停止規則
 func stop_spin():
@@ -92,5 +92,3 @@ func stop_spin():
 		stop_tween.set_trans(Tween.TRANS_QUAD)
 		stop_tween.set_ease(Tween.EASE_OUT)
 		stop_tween.tween_property(reel,"position:y" , tareget_y,0.2)
-		
-	prize_number = (turn_list[prize_index])
